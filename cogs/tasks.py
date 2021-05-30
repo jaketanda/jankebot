a617a2457c3cea12f2e9c4b3067c17cca216ad5e
jaketanda <jakemtanda@gmail.com>
2021-05-30 17:28:12 -0400

    Fix error where tasks looks for client before initialization

--- cogs/tasks.py
+++ cogs/tasks.py
@@ -5,13 +5,15 @@ import json
 import random
 import datetime
 
-with open('././config.json', "r") as configFile:
-    config = json.load(configFile)
-    status = cycle(config.get("statuses"))
-
 class Tasks(commands.Cog):
     def __init__(self, client):
         self.client = client
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        with open('././config.json', "r") as configFile:
+            config = json.load(configFile)
+            self.status = cycle(config.get("statuses"))
         self.change_status.start()
 
     def cog_unload(self):
@@ -19,8 +21,7 @@ class Tasks(commands.Cog):
 
     @tasks.loop(seconds=1800)
     async def change_status(self):
-        print('Changing status...')
-        await self.client.change_presence(activity=discord.Game(next(status)))
+        await self.client.change_presence(activity=discord.Game(next(self.status)))
 
 
 def setup(client):
